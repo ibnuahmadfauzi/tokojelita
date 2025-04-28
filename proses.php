1a7573f9949f5194b55c9fbb9fcb4a9ca72c95f5
ibnuahmadfauzi <ibnu7897@gmail.com>
2025-04-28 20:50:42 +0700

    edit code

--- proses.php
+++ proses.php
@@ -1,29 +1,29 @@
 <?php
 include 'koneksi.php';
 
-// Ambil kategori dari database
+// mengambil data kategori dari database dan memasukkan ke array kategori
 $kategori = [];
 $qKategori = mysqli_query($koneksi, "SELECT * FROM kategori");
 while ($row = mysqli_fetch_assoc($qKategori)) {
     $kategori[] = $row;
 }
 
-// Proses form submit
+// proses submit
 $rekomendasi = [];
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nama = $_POST['nama'];
     $kategori_disukai = isset($_POST['kategori']) ? $_POST['kategori'] : [];
 
-    // Simpan user baru
+    // mengambil nilai nama user baru
     mysqli_query($koneksi, "INSERT INTO users (nama) VALUES ('$nama')");
     $user_id_baru = mysqli_insert_id($koneksi);
 
-    // Simpan kategori yang disukai
+    // mengambil kategori yang disukai
     foreach ($kategori_disukai as $id_kategori) {
         mysqli_query($koneksi, "INSERT INTO user_kategori (user_id, kategori_id) VALUES ($user_id_baru, $id_kategori)");
     }
 
-    // Ambil semua user
+    // mengambil semua data user
     $users = [];
     $qUsers = mysqli_query($koneksi, "SELECT * FROM users");
     while ($row = mysqli_fetch_assoc($qUsers)) {
@@ -33,18 +33,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         ];
     }
 
-    // Ambil semua user_kategori
+    // mengambil semua data user kategori
     $qUserKategori = mysqli_query($koneksi, "SELECT * FROM user_kategori");
     while ($row = mysqli_fetch_assoc($qUserKategori)) {
         $users[$row['user_id']]['kategori'][] = $row['kategori_id'];
     }
 
-    // Hitung kemiripan (Cosine Similarity sederhana)
+    // menghitung kemiripan (Cosine Similarity sederhana)
     $kemiripan_tertinggi = 0;
     $user_mirip_id = 0;
 
     foreach ($users as $id => $data) {
-        if ($id == $user_id_baru) continue; // Jangan dibandingkan dengan diri sendiri
+        if ($id == $user_id_baru) continue;
 
         $kategori_lama = $data['kategori'];
         $kategori_baru = $users[$user_id_baru]['kategori'];
@@ -62,7 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     }
 
-    // Cari kategori yang belum dipilih user baru tapi dipilih user mirip
+    // mencari kategori yang belum dipilih user baru tapi dipilih user mirip
     if ($user_mirip_id != 0) {
         $kategori_mirip = $users[$user_mirip_id]['kategori'];
         foreach ($kategori_mirip as $p) {
